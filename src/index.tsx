010bdf0f70fcb74ed79fe5c26e684e2512c3e4cc
dohaicuong <beatyshot@gmail.com>
2020-05-21 22:43:53 +1000

    init project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from 'serviceWorker'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { Auth0Provider, useAuth0 } from 'providers/AuthProvider'
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_IDENTIFIER } from 'configs'
+import RelayProvider from 'providers/RelayProvider'
+import PrivateRoute from 'components/PrivateRoute'
+
+const AppBar = () => {
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0()
+
+  return (
+    <div>
+      {!isAuthenticated
+        ? (loginWithRedirect && <button onClick={() => loginWithRedirect()}>Login</button>)
+        : (logout && <button onClick={() => logout()}>Logout</button>)
+      }{' '}
+      {isAuthenticated && (
+        <span>
+          <Link to='/'>Home</Link>{' '}
+          <Link to='/profile'>Profile</Link>
+        </span>
+      )}
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Auth0Provider domain={AUTH0_DOMAIN} client_id={AUTH0_CLIENT_ID} audience={AUTH0_IDENTIFIER}>
+        <React.Suspense fallback='Root loading...'>
+          <RelayProvider>
+            <AppBar />
+            <Switch>
+              <Route exact path='/' component={React.lazy(() => import('pages/Home'))} />
+              <PrivateRoute exact path='/profile' component={React.lazy(() => import('pages/Profile'))} />
+            </Switch>
+          </RelayProvider>
+        </React.Suspense>
+      </Auth0Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
+serviceWorker.unregister()
